d1fbd644e20fa46b367019dd8e6b92e405d936bd
RiotHelmet <85107883+RiotHelmet@users.noreply.github.com>
2023-05-23 23:17:39 +0200

    Delete userdata_export_2023-05-22_230119.sql

--- userdata_export_2023-05-22_230119.sql
+++
@@ -1,5 +0,0 @@
-insert into `userdata` (`kills`, `password`, `userID`, `username`) values (2, '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', 2, 'admin');
-insert into `userdata` (`kills`, `password`, `userID`, `username`) values (2, '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', 3, 'admin2');
-insert into `userdata` (`kills`, `password`, `userID`, `username`) values (0, '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', 4, 'admin3');
-insert into `userdata` (`kills`, `password`, `userID`, `username`) values (0, '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', 5, 'admin4');
-insert into `userdata` (`kills`, `password`, `userID`, `username`) values (0, '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', 6, 'admin1');
